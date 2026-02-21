467d476d050a1334c74f2c36e6bc1ade1549f0e6
Aditya Kumar <way2adityakumar@gmail.com>
2026-02-21 22:43:26 +0530

    final changes

--- src/hooks/useUpcomingMovies.jsx
+++ src/hooks/useUpcomingMovies.jsx
@@ -11,7 +11,7 @@ export const useUpcomingMovies = () => {
   const dispatch = useDispatch();
   const fetchUpcomingMovies = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/upcoming?page=1",
+      "https://corsproxy.io/?https://api.themoviedb.org/3/movie/upcoming?page=1",
       API_OPTIONS,
     );
     const json = await data.json();
