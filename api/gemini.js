1ec9a0d519768ee5a209c68a37c86fe892db6fff
Aditya Kumar <way2adityakumar@gmail.com>
2026-02-25 22:49:11 +0530

    changed gemini model to gemini-3-flash

--- api/gemini.js
+++ api/gemini.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     const { prompt } = req.body;
 
     const result = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-3-flash-preview",
       contents: prompt,
     });
 
